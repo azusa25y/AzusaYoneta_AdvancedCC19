1fcce7229b912e19a481e4887e4e204cf7c72dfb
azusa25y <31422660+azusa25y@users.noreply.github.com>
2019-02-12 18:52:40 -0500

    Update ffmpeg_Azusa Yoneta.sh

--- ffmpeg_Azusa Yoneta.sh
+++ ffmpeg_Azusa Yoneta.sh
@@ -1,4 +1,4 @@
-//Shorten the videos
+#Shorten the videos
 ffmpeg -i computerworld.mp4 -ss 00:02 -to 00:46 -c:v copy -c:a copy azusa_computerworld.mp4
 ffmpeg -i computerworld.mp3 -ss 00:11 -to 00:51 -c:v copy -c:a copy computerworld4.mp3
 ffmpeg -i computerworld.mp3 -ss 00:11 -to 00:51 -c:v copy -c:a copy computerworld4.mp3
@@ -16,7 +16,7 @@ ffmpeg -i art1.mp4 -ss 05:34 -to 05:41 -c:v copy -c:a copy back4.mp4
 ffmpeg -i art1.mp4 -ss 04:03 -to 04:10 -c:v copy -c:a copy back6.mp4
 ffmpeg -i art1.mp4 -ss 01:30 -to 01:38 -c:v copy -c:a copy back5.mp4
 
-//Concatenate 
+#Concatenate 
 ffmpeg -i first.mp4 -i history4.mp4 -i history5.mp4 \
   -filter_complex "[0:v] [0:a] [1:v] [1:a] [2:v] [2:a] concat=n=3:v=1:a=1 [v] [a]" \
   -map "[v]" -map "[a]" first2.mp4
@@ -25,10 +25,10 @@ ffmpeg -i background1.mp4 -i back4.mp4 -i back2.mp4 \
   -filter_complex "[0:v] [0:a] [1:v] [1:a] [2:v] [2:a] concat=n=3:v=1:a=1 [v] [a]" \
   -map "[v]" -map "[a]" background3.mp4
 
-//Concatenate with txt file
+#Concatenate with txt file
 ffmpeg -f concat -safe 0 -i mylist2.txt -c copy concat_output.mov
 
-//Overlayed Two Videos
+#Overlayed Two Videos
 ffmpeg \
 -i computerworld_video.mp4 -i background3.mp4 \
 -filter_complex " \
@@ -38,10 +38,10 @@ format=yuva420p, colorchannelmixer=aa=0.5[bottom]; \
 [top][bottom]overlay=shortest=1" \
 -vcodec libx264 finalvideo2.mp4
 
-//Fade out the audio of my videos
+#Fade out the audio of my videos
 ffmpeg -i azusa_final.mp4 -af afade=t=in:st=0:d=3,afade=t=out:st=35:d=5 azusa_final_fade.mp4
 
-//Code I tried out but didnot use for final video
+#Code I tried out but didnot use for final video
 ffmpeg -i 'space-%03d.png' -c:v libx264 -pix_fmt yuv420p space_q.mp4
 ffmpeg -i 'vr-%03d.png' -c:v libx264 -pix_fmt yuv420p vr_q.mp4
 ffmpeg  -i vr.mp4 -vf fps=1 vr-%03d.png
